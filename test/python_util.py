c2242443f8cdc1271b50b642dd2727ef89a92627
yuzhongchun <yuzhongchun@baidu.com>
2019-02-20 17:59:58 +0800

    add hands-on-ml scripts

---
+++ test/python_util.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*ﬁ-
+"""
+ @FileName: python_util
+ @Desc:  
+ @Author: yuzhongchun
+ @Date: 2019-02-20 17:21
+ @Last Modified by: yuzhongchun
+ @Last Modified time: 2019-02-20 17:21
+"""
+
+import numpy as np
+import matplotlib.pylab as plt
+import pandas as pd
+
+# for i in range(1, 1001):
+#     num = np.ceil(np.random.uniform(0, 10000))
+#     print(i, num)
+
+rand_data = np.random.randint(0, 10000, 200)
+# print(rand_data)
+# print(type(rand_data))
+
+rand_data_frame = pd.DataFrame(rand_data)
+# print(rand_data_frame)
+
+repeat_data = rand_data_frame[0].value_counts()
+
+# print(repeat_data)
+# print(type(repeat_data))
+
+count = 0
+for index, value in enumerate(repeat_data):
+    if value > 1:
+        count += 1
+        # print(index, value)
+
+print("count =", count)
+
+# n, bins, patches = plt.hist(rand_data, bins=40)
+# plt.grid(True)
+# plt.show()
